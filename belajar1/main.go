df0e8f03b90173a5012d6fa814bb4675b30b1a54
venomcodegithub <43227188+venomcodegithub@users.noreply.github.com>
2018-09-13 11:51:15 +0800

    Add files via upload

---
+++ belajar1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+  "net/http"
+  "github.com/gorilla/mux"
+  "html/template"
+)
+
+var templates *template.Template
+
+
+func main(){
+      templates = template.Must(template.ParseGlob("templates/*.html"))
+      r := mux.NewRouter()
+      r.HandleFunc("/", indexHandler).Methods("GET")
+      http.Handle("/", r)
+      fs := http.FileServer(http.Dir("./static/"))
+      r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+      http.ListenAndServe(":8080", nil)
+  }
+
+
+  func indexHandler(w http.ResponseWriter, r *http.Request){
+    templates.ExecuteTemplate(w, "index.html", nil)
+  }
